e24a372cdc218b2a8c7fcb9969a2532344478ec7
Harry Ho <TheTitan8706@gmail.com>
2015-05-29 15:25:21 -0400

    feat(comment) adds a comment component

---
+++ app/lib/comment/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'test-object-comment';
+
+export default class Comment extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  //Render Function
+  render () {
+    const model = this.props.model;
+    return (
+      <div className='comment-block'>
+        <div className='comment-block__header background-grey text-orange'>
+          {model.title}
+        </div>
+        <div className='comment-block__body'>
+          "{model.comment}"
+        </div>
+        <div className='comment-block__footer'>
+          By {model.user}
+        </div>
+      </div>
+    );
+  }
+}
