f669ab9c545a0bb15ea0aa7b04ea0c9590ea2eb2
Harry Ho <TheTitan8706@gmail.com>
2015-05-29 15:26:14 -0400

    mock(mock-comments) adds mock data for comments

---
+++ app/lib/content/mock-comments.js
@@ -0,0 +1,30 @@
+
+const MOCK_DATA = [
+  {
+    "id" : 5,
+    "title": "Fish Is Good",
+    "comment": "I sure do like fish",
+    "user" : "Harry Ho"
+  },
+  {
+    "id" : 5,
+    "title": "Cheese",
+    "comment": "I am also a fan of cheese",
+    "user" : "Harry Ho"
+  },
+  {
+    "id" : 5,
+    "title": "Combination",
+    "comment": "However, fish and cheese is only sometimes okay",
+    "user" : "Harry Ho"
+  },
+  {
+    "id" : 5,
+    "title": "Moo",
+    "comment": "Moo",
+    "user" : "Random Cow"
+  }
+
+];
+
+export default MOCK_DATA;
