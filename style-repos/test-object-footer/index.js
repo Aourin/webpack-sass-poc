4f200001229d7699838661396728c4c39f5773e4
Harry Ho <TheTitan8706@gmail.com>
2015-05-29 15:53:26 -0400

    docs(test-object-footer) updates comments for footer object styles

--- style-repos/test-object-footer/index.js
+++ style-repos/test-object-footer/index.js
@@ -1,3 +1,6 @@
+//Requires a shared background-red style
+//Requires test-footer-theme sub-module whic has shared deps too
+
 require('test-background-red');
 require('test-footer-theme');
 require('./style.scss');
