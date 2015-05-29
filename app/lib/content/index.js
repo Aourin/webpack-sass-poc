aaac49bddeddc6a30d9b9174aaca41bddd476caf
Harry Ho <TheTitan8706@gmail.com>
2015-05-29 15:27:21 -0400

    refactor(ContentBlock) adds mock to content block and comment stream

--- app/lib/content/index.js
+++ app/lib/content/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {default as comments} from './mock-comments';
+import Comment from 'lib/comment';
 
+//Default ContentBlock class
 export default class ContentBlock extends React.Component {
   constructor (props) {
     super(props);
   }
+
   render () {
+    //Create a Comment Collection
+    const CommentList = this.comments.map((comment) => {
+      return <Comment model={comment}/> 
+    }, comments);
+
+    //Return content-block
     return (
       <div className='content-block'>
         This is a bunch of content in this content pane
+        {CommentList}
       </div>
     );
   }
