54fefa230dd3bac58d894961b35176e7531b30f9
Harry Ho <TheTitan8706@gmail.com>
2015-06-04 12:47:10 -0400

    refactor(style-repos) renamed and moved style repos

--- style-repos/test-object-comment/index.js
+++
@@ -1,8 +0,0 @@
-//Requires test-header-theme repo
-//test-header-theme deps
-//--test-background-grey
-//--test-color-orange
-
-require('test-header-theme');
-require('./style.scss');
-
--- style-repos/test-object-footer/index.js
+++
@@ -1,6 +0,0 @@
-//Requires a shared background-red style
-//Requires test-footer-theme sub-module whic has shared deps too
-
-require('test-background-red');
-require('test-footer-theme');
-require('./style.scss');
